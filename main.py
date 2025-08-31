3f9de4a775fef4b1ec209e5f2fe6df9951abc699
MinerFarmIsCool <joshua_whelan@outlook.com>
2025-08-31 21:13:27 +1000

    refactored functions into a different file updated .git ignore to add pycache

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
+from stats import get_book_text, get_book_word_count
 
 
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-        return file_contents
-    
-def get_book_word_count(book):
-    book_word_list = book.split()
-    return len(book_word_list)
-    
 def main():
     frankenstein_text = get_book_text("books/frankenstein.txt")
     frankenstein_word_count = get_book_word_count(frankenstein_text)
