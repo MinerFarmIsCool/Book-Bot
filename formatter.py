c337a9873111e493743827c296419945bba610ec
MinerFarmIsCool <joshua_whelan@outlook.com>
2025-09-01 14:14:00 +1000

    Add formatter module to print out a formatted message with the report and update main to use print_the_formatted_stuff

---
+++ formatter.py
@@ -0,0 +1,11 @@
+from stats import get_stats
+
+def print_the_formatted_stuff(filepath):
+    book, book_word_count, book_letter_count = get_stats(filepath)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {book_word_count} total words")
+    print("--------- Character Count -------")
+    for dict in book_letter_count:
+        print(f"{dict['char']}: {dict['count']}")
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from stats import get_book_text, get_book_word_count, get_letter_counts
+from formatter import print_the_formatted_stuff
 
 
-def main():
+def not_main():
     frankenstein_text = get_book_text("books/frankenstein.txt")
     frankenstein_word_count = get_book_word_count(frankenstein_text)
     print(f"{frankenstein_word_count} words found in the document")
     frankenstein_letter_dict = get_letter_counts(frankenstein_text)
     print(frankenstein_letter_dict)
+
+def main():
+    print_the_formatted_stuff("books/frankenstein.txt")
+
 
 main()
