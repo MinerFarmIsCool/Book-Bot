010bd2ff8e1eb8a3653b9c63a5bb5aa209314487
MinerFarmIsCool <joshua_whelan@outlook.com>
2025-09-01 13:31:22 +1000

    Added a call of the get_letter_counts function

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ def main():
     frankenstein_text = get_book_text("books/frankenstein.txt")
     frankenstein_word_count = get_book_word_count(frankenstein_text)
     print(f"{frankenstein_word_count} words found in the document")
+    frankenstein_letter_dict = get_letter_counts(frankenstein_text)
+    print(frankenstein_letter_dict)
 
 main()
