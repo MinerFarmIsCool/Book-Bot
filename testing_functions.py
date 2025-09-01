bb5f4d4224561e677164e6e37b65643b12925c38
MinerFarmIsCool <joshua_whelan@outlook.com>
2025-09-01 13:25:13 +1000

    Added testing functions file to use as a testing file instead of modifying main

---
+++ testing_functions.py
@@ -0,0 +1,8 @@
+from stats import get_book_text, get_book_word_count, get_letter_counts
+
+def main():
+    sentance = "The quick brown fox"
+    weeee = get_letter_counts(sentance)
+    print(weeee)
+
+main()
